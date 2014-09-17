7c6ac56ab235fbe7a34a8a5962716ecbcdfaae5a
Ayo Fapohunda <oaf2119@columbia.edu>
2014-09-16 20:03:07 -0400

    modified program to use arrays to reverse print

--- part2/convert.c
+++ part2/convert.c
@@ -6,13 +6,14 @@ int main(){
  	
 	int x;
 
- 	printf("Please enter an integer\n");
+ 	//printf("Please enter an integer\n");
  	scanf("%d", &x); 
 	
 	
-    	printf("signed dec:	 %%d: %d\n", x);
-    	printf("unsigned dec:	 %%u: %u\n", x);
-    	printf("hex:		 %%x: %x\n", x);
+    	printf("signed dec:   %d\n", x);
+    	printf("unsigned dec: %u\n", x);
+    	printf("hex:	      %x\n", x);
+	printf("binary:	      ");	
 	printbinary(x);
 	printf("\n");
 
@@ -26,14 +27,37 @@ int main(){
 void printbinary (int x){
 
 	int i;
+	int bitArray[32];
+	int counter =1;
 
 	for( i=0; i < 32; i++){
-		if(x%2 == 0) {
-			printf("%d", 0);
+
+		if(x >= 0) {
+
+			if(x%2 == 0) {
+				bitArray[i] = 0;	//printf("%d", 0);
+			} else {
+				bitArray[i] = 1;	//printf("%d", 1);
+			}
+			x = x/2;
+
+			//if(counter % 4 ==0) printf(" ");
+			//counter++;
 		} else {
-			printf("%d", 1);
+			//x = -1 * x;
+			//int y;
+			//if(x%2
+
+
 		}
-		
-		//printf("binary: %d  \n", x);
+
 	}
-}
+	for (i = 31; i >= 0; i--){
+		printf("%d", bitArray[i]);
+		if(counter % 4 ==0) printf(" ");
+		counter++;
+	}	
+
+
+	}
+	
