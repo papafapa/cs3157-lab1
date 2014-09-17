129a8b58d7486a8c57638f3e3d406b39d1c70498
Ayo Fapohunda <oaf2119@columbia.edu>
2014-09-16 22:23:37 -0400

    got the program working properly

--- part2/convert.c
+++ part2/convert.c
@@ -26,38 +26,44 @@ int main(){
 
 void printbinary (int x){
 
-	int i;
+	
 	int bitArray[32];
 	int counter =1;
 
-	for( i=0; i < 32; i++){
+	int i;
 
-		if(x >= 0) {
+	if (x >= 0){
+
+		for( i=0; i < 32; i++){
 
 			if(x%2 == 0) {
-				bitArray[i] = 0;	//printf("%d", 0);
+				bitArray[i] = 0;
 			} else {
-				bitArray[i] = 1;	//printf("%d", 1);
+				bitArray[i] = 1;	
 			}
 			x = x/2;
-
-			//if(counter % 4 ==0) printf(" ");
-			//counter++;
-		} else {
-			//x = -1 * x;
-			//int y;
-			//if(x%2
-
-
 		}
 
+	} else {
+		x = - 1 * x;
+		int y = 1;
+
+		for( i = 0; i < 32; i++){
+		
+			if(x%2 == 0) {
+				bitArray[i] = 1 - y;        
+			} else {
+				bitArray[i] = 0 + y;
+				if (y == 1) y = y - 1;	
+			}
+			x = x/2; 
+		}
 	}
+
 	for (i = 31; i >= 0; i--){
 		printf("%d", bitArray[i]);
 		if(counter % 4 ==0) printf(" ");
 		counter++;
-	}	
-
-
-	}
+		}	
+}
 	
